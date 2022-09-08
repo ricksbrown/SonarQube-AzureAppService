34d9c14dc0daf0fb26c7e32cba27a9a6724eaf76
Rick Brown <ricksbrown73@gmail.com>
2022-09-09 09:21:09 +1000

    Install JQ

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo 'Install JQ'
+wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+chmod +x ./jq
+mv jq /usr/bin
+
 echo 'Copy wwwroot folder'
 copy -r wwwroot ../wwwroot
