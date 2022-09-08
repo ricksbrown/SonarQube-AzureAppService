51e8f2a4dcae2f35969fc091aecca1dc6002599b
Rick Brown <ricksbrown73@gmail.com>
2022-09-09 09:40:50 +1000

    Echo env vars

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "DEPLOYMENT_SOURCE: $DEPLOYMENT_SOURCE"
+echo "DEPLOYMENT_TARGET: $DEPLOYMENT_TARGET"
+
 echo 'Install JQ'
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-chmod +x ./jq
+chmod u+x ./jq
 mv jq /usr/bin
 
 echo 'Copy wwwroot folder'
-copy -r wwwroot ../wwwroot
+copy -r /home/site/repository/wwwroot /home/site/wwwroot/wwwroot
