804ff9d82c9ab10487df7352c2ea5ca9a89aeff5
Rick Brown <ricksbrown73@gmail.com>
2022-09-09 09:51:30 +1000

    Fix copy command

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 chmod u+x ./jq
 
 echo "Copy $DEPLOYMENT_SOURCE/wwwroot folder to $DEPLOYMENT_TARGET" 
-cp -r $DEPLOYMENT_SOURCE/wwwroot $DEPLOYMENT_TARGET
+cp -r $DEPLOYMENT_SOURCE/wwwroot/** $DEPLOYMENT_TARGET
