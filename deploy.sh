17308575c4b4ffb4e80c08a0311e57af06f61115
Rick Brown <ricksbrown73@gmail.com>
2022-09-09 09:46:17 +1000

    Fix typo  in copy command

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 echo 'Install JQ'
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+chmod u+x ./jq
 
 echo "Copy $DEPLOYMENT_SOURCE/wwwroot folder to $DEPLOYMENT_TARGET" 
-copy -r $DEPLOYMENT_SOURCE/wwwroot $DEPLOYMENT_TARGET
+cp -r $DEPLOYMENT_SOURCE/wwwroot $DEPLOYMENT_TARGET
