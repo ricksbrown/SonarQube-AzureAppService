ff99f52df30e7b0a2d730908ceb5ffe3ca437b29
Rick Brown <ricksbrown73@gmail.com>
2022-09-09 09:45:07 +1000

    Use env vars

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "DEPLOYMENT_SOURCE: $DEPLOYMENT_SOURCE"
-echo "DEPLOYMENT_TARGET: $DEPLOYMENT_TARGET"
-
 echo 'Install JQ'
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-chmod u+x ./jq
-mv jq /usr/bin
 
-echo 'Copy wwwroot folder'
-copy -r /home/site/repository/wwwroot /home/site/wwwroot/wwwroot
+echo "Copy $DEPLOYMENT_SOURCE/wwwroot folder to $DEPLOYMENT_TARGET" 
+copy -r $DEPLOYMENT_SOURCE/wwwroot $DEPLOYMENT_TARGET
