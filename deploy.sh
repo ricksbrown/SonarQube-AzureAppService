3ad049304c29b9008eced31b84aeaf79c4c4283c
Rick Brown <ricksbrown73@gmail.com>
2022-09-09 12:42:18 +1000

    Add more logging

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "User `whoami`"
 echo 'Install JQ'
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 chmod u+x ./jq
@@ -8,7 +8,9 @@ chmod u+x ./jq
 # cp -r $DEPLOYMENT_SOURCE/wwwroot/** $DEPLOYMENT_TARGET
 
 sonar_version='9.6.1.59531'
-wget "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${sonar_version}.zip"
+wget --no-hsts "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${sonar_version}.zip"
 unzip "sonarqube-${sonar_version}.zip"
 export SONARQUBE_HOME="`pwd`/sonarqube-${sonar_version}"
+
+echo "Running ${SONARQUBE_HOME}/bin/linux-x86-64/sonar.sh"
 $SONARQUBE_HOME/bin/linux-x86-64/sonar.sh console
